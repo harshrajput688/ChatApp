f6965842291c020eaf3899b7ff5d71c16c3a83f2
Harsh Rajput <77964116+harshrajput688@users.noreply.github.com>
2025-05-01 23:25:42 +0530

    projectsetup

---
+++ ChatApp/ChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatApp
+//
+//  Created by Harsh Rajput on 01/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ChatApp/ChatAppTests/ChatAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChatAppTests.swift
+//  ChatAppTests
+//
+//  Created by Harsh Rajput on 01/05/25.
+//
+
+import Testing
+
+struct ChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
